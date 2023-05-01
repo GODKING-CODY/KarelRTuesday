82f4477f37d256f3ee7f2bdff6a8f19fcdddc7ce
Mark Lessard <nlessard@mon-avenir.ca>
2023-05-01 12:15:32 -0400

    w
    w

--- tasks/inerhitor.rb
+++ tasks/inerhitor.rb
@@ -1,32 +1,30 @@
 #Copyright 2010 Joseph Bergin
 #License: Creative Commons Attribution-Noncommercial-Share Alike 3.0 United States License
-
-
+ 
+require_relative "stair_sweeper"
 require_relative "../karel/ur_robot"
 require_relative "../mixins/turner"
 # A class whose robots know how to sweep a short staircase of beepers
-class StairSweeper < UrRobot
+class Inerhitor < StairSweeper
   include Turner
   def initialize (street, avenue, direction, beepers)
     super(street, avenue, direction, beepers)
   end
   
-  def mover_beeper
-    put_beeper
+  def square_beeper
+    mover_beeper
+    mover_beeper
+    mover_beeper
+    mover_beeper
+    mover_beeper
+    turn_left
+    turn_left
+    move_five
+    turn_left
     move
-  end
-  def mover_beeperback
-    put_beeper
-    move
-  end
-  def move_five
-    move
-    move
-    move
-    move
-    move
-  end
-    
-  
+    turn_left
 
+   end 
+  
+   	
 end
